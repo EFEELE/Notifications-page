f053828fd14ec93a9c6e2c5f8e7825958686a4b7
EFEELE <ferbock10@gmail.com>
2024-06-04 15:31:27 -0600

    fix counter

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
+let elmentsUnRead = document.getElementsByClassName('unread');
+let tagCount = document.getElementById('count')
+let count;
+
+function countUpdate(){
+    count = elmentsUnRead.length;
+    tagCount.innerHTML = count;
+}
+countUpdate();
+
 function read(event){
    let element = event.currentTarget;
    element.classList.remove('unread');
+   countUpdate();
 }
 
 function allRead(){
-    let elmentsUnRead = document.getElementsByClassName('unread');
-
     while(elmentsUnRead){
         elmentsUnRead[0].classList.remove('unread')   
+        countUpdate();
     }
-
 }
